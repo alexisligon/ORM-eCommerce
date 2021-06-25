efbfd00cd6d7f08a1aa1854d5f1ad48758b93665
alexisligon <aligo@macbook-pro.home>
2021-06-25 12:04:43 -0400

    product.js model columns defined

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validates that the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //set as default value of 10
+      //validates that the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references the category model's id
+    }
   },
   {
     sequelize,
