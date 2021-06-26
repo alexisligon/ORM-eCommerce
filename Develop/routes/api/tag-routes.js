23cc8dfb709f22a39298e815ab76ce1e3c54b09d
alexisligon <aligo@macbook-pro.home>
2021-06-26 09:55:28 -0400

    get tag by id works

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
+//I ADDED THIS
 
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  //I ADDED THIS
   try {
     const tagData = await Tag.findAll({
       
@@ -18,9 +18,18 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id);
+    if(!tagData) {
+      res.status(404).json({message: 'No tag found with this id'})
+    }
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
