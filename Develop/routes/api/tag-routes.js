30fa6b4901a64f88de5f6b0aa05f11baafa50948
alexisligon <aligo@macbook-pro.home>
2021-06-28 11:58:28 -0400

    delete tag functional

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -9,10 +9,10 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      
+
     })
     res.status(200).json(tagData);
-  }catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 
@@ -23,8 +23,8 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id);
-    if(!tagData) {
-      res.status(404).json({message: 'No tag found with this id'})
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id' })
     }
     res.status(200).json(tagData)
   } catch (err) {
@@ -44,11 +44,25 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id' })
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
