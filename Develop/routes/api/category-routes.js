592ec9625e48692b2986b8cf574c4aa9df2203d8
alexisligon <aligo@macbook-pro.home>
2021-06-28 11:53:50 -0400

    post new tags functional

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -19,7 +19,6 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  //THIS IS NOT WORKING!!!!
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -39,12 +38,22 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData)
   } catch (err) {
-    res.status(400).json(err)
+    res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updateCategory)
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
